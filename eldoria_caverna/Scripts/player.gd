446188919a0e15e598e88f1039291e12717a320a
dudualbertti <dudsalbertti@gmail.com>
2025-04-05 17:13:24 -0300

    teste

--- eldoria_caverna/Scripts/player.gd
+++ eldoria_caverna/Scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-#hfghfhgfhgfhg
+#hfghfhgfhgfhglkjklj
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
